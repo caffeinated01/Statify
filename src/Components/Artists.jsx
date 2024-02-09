9ad1cd7fdf6014365dc9a055450aac18d1529d92
Javier <javier.has.no.life@gmail.com>
2024-02-10 00:33:06 +0800

    add ability to  change time range

--- src/Components/Artists.jsx
+++ src/Components/Artists.jsx
@@ -1,12 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import LoginContext from "../LoginContext";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, ChevronDown } from "lucide-react";
 
 function Artists(){
     const loginContext = useContext(LoginContext);
     const token = localStorage.getItem("token");
     const [artistsJSON, setArtistsJson] = useState([])
+    const [timeRange, setTimeRange] = useState("short_term")
 
     useEffect(()=>{
         async function fetchTopArtists(){ 
@@ -15,7 +16,7 @@ function Artists(){
                     Authorization: `Bearer ${token}`
                 },
                 params: {
-                        time_range: "short_term", // TODO: Make this changeable with a dropdown menu or smt (short_term, medium_term, long_term)
+                        time_range: timeRange, // TODO: Make this changeable with a dropdown menu or smt (short_term, medium_term, long_term)
                         limit: 30,
                         offset: 0
                 }
@@ -23,7 +24,11 @@ function Artists(){
             setArtistsJson(data.items)
         };
         fetchTopArtists();
-    }, []);
+    }, [timeRange]);
+
+    function handleTimeRangeChange(e){
+        setTimeRange(e.target.value);
+    }
 
     // TODO: Add dropdown menu to change time range
     
@@ -31,6 +36,11 @@ function Artists(){
         <>
             <div className="flex flex-col items-center justify-center gap-10 py-10">
                 <h1 className="text-2xl">Top Artists</h1>
+                <select onChange={handleTimeRangeChange} className="h-10 px-3 text-white bg-bg-secondary rounded-md border-[1px] border-[#ffffff1a] appearance-none">
+                    <option value="short_term">Last 4 weeks </option>
+                    <option value="medium_term">Last 6 months</option>
+                    <option value="long_term">All time</option>
+                </select>
                 <div className="flex flex-wrap items-center justify-center">
                     {artistsJSON.map((artist, index) => (
                         <div key={index} className="mx-4 my-4 bg-bg-secondary relative flex flex-col items-center justify-center gap-2 rounded-md border-[1px] border-[#ffffff1a] px-5 py-5 shadow-2xl">
